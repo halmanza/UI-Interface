412407f7a46eb753a2393d8628a3b2b994e507a0
Anthony Almanza <anthonya771@gmail.com>
2021-07-17 13:01:58 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import Libraries from "./pages/Libraries";
+import cn from "classnames";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import * as style from '../src/styles/app.module.css';
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  // Dimms the rest of the screen while SideBar is open
+  const classes = cn("pusher", "bottom", { dimmed: toggle });
+
+  const toggleMenu = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={style}>
+        <Header onToggleMenu={toggleMenu} />
+
+        <div className="ui attached pushable" style={{ height: "100vh" }}>
+          <Sidebar toggleMenu={toggle} />
+
+          <div className={classes}>
+            <Main>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/contact">
+                <Contact />
+              </Route>
+              <Route exact path="/libraries">
+                <Libraries />
+              </Route>
+            </Main>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
